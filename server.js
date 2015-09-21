f8d7130d452f6bc7dee39b458e40d50687a27d6e
Max Tedford <max.tedford@gmail.com>
2015-09-21 09:26:31 -0600

    add root directory

--- server.js
+++ server.js
@@ -4,3 +4,7 @@ const express = require('express');
 const app = express();
 
 app.use(express.static("public"));
+
+app.get("/", function( req, req){
+  res.sendFile(__dirname + "/public/index.html")
+});
