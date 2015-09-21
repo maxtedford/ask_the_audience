594c255f7fe041e30ad6f40679577b0c561632e9
Max Tedford <max.tedford@gmail.com>
2015-09-21 09:47:07 -0600

    fix typo

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get("/", function( req, res){
   res.sendFile(__dirname + "/public/index.html")
 });
 
-const port = process.eng.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 const server = http.createServer(app)
   .listen(port, function() {
