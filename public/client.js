bbccdc838726b0695c09786ba6ac9a4749dd53fc
Max Tedford <max.tedford@gmail.com>
2015-09-21 11:13:05 -0600

    add event listeners for all buttons

--- public/client.js
+++ public/client.js
@@ -2,6 +2,7 @@ var socket = io();
 
 var connectionCount = document.getElementById('connection-count');
 var connectionWelcome = document.getElementById('connection-welcome');
+var buttons = document.querySelectorAll('#choices button');
 
 socket.on('usersConnected', function(count) {
   connectionCount.innerText = 'Connected Users: ' + count;
@@ -10,3 +11,9 @@ socket.on('usersConnected', function(count) {
 socket.on('statusMessage', function(message) {
   connectionWelcome.innerText = message;
 });
+
+for(var i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', function() {
+    console.log(this.innerText);
+  })
+}
