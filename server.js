b83b873b619be4bdc6ac2a568eae79888074067d
Max Tedford <max.tedford@gmail.com>
2015-09-21 09:39:42 -0600

    set listening port to 3000

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const app = express();
 
 app.use(express.static("public"));
 
-app.get("/", function( req, req){
+app.get("/", function( req, res){
   res.sendFile(__dirname + "/public/index.html")
+});
+
+var server = http.createServer(app)
+  .listen(3000, function() {
+    console.log('Listening on port 3000.');
 });
